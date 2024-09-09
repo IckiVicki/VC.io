64d2a361f409d0b2b7857381fd9f561ba401c13a
IckiVicki <155053542+IckiVicki@users.noreply.github.com>
2024-09-09 18:23:15 -0400

    Add files via upload
    The Finished Product.

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, url_for, request
+
+app = Flask(__name__)
+DEBUG = True
+SECRET_KEY = 'secret key'
+app.config.from_object(__name__)
+def base():
+    return render_template('base.html') 
+
+@app.route('/', methods=['GET'])
+@app.route('/home', methods=['GET'])
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app(debug=True)
